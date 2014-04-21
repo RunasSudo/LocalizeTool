de6468e92f6f7c4855e07fa26f43c1a83675566d
RunasSudo <RunasSudo@gmail.com>
2014-04-21 15:25:10 +0930

    Progress window hides on close.

--- src/io/github/runassudo/localize/ProgressWindow.java
+++ src/io/github/runassudo/localize/ProgressWindow.java
@@ -19,7 +19,7 @@ public class ProgressWindow extends JFrame {
 	 */
 	public ProgressWindow() {
 		setTitle("Status");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setBounds(100, 100, 450, 74);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
